6d035aa0843acfd23bd8c3bc4f7139f8df785c51
Tim <tim.matzenauer@outlook.de>
2024-08-02 16:28:53 +0200

    Removed Data Content - Not needed

--- PSXenforo/Public/ps1/Set-XenforoThreadRead.ps1
+++ PSXenforo/Public/ps1/Set-XenforoThreadRead.ps1
@@ -5,12 +5,6 @@ Function Set-XenforoThreadRead{
 
         [ValidateNotNullOrEmpty()] [string] $UserId
     )
-
-    $Data = @{
-        "node_id"   = $NodeId
-        "title"     = $Title
-        "message"   = $Content
-    }
     
     if($UserId){
         return Invoke-XenforoRequest -Method Post -Resource "/threads/$($ThreadId)/mark-read" -UserId $UserId
